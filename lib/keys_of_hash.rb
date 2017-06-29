4846b3bd485f9ade29d4c9ccadee1889e3506a5f
Kenny Lin <kenny.lin.91@gmail.com>
2017-06-29 13:01:02 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,18 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(arguments, *another)
+    if another
+      another = another.join("")
+      if arguments.is_a?(Integer)
+        another = another.to_i
+        array1 = self.collect { |x,y|  x if y == arguments}.compact
+        array2 = self.collect { |x,y|  x if y == another}.compact
+        return array1.concat(array2)
+      else
+        array1 = self.collect { |x,y|  x if y == arguments}.compact
+        array2 = self.collect { |x,y|  x if y == another}.compact
+        return array1.concat(array2)
+      end
+    end
+    self.collect { |x,y|  x if y == arguments}.compact
   end
 end
